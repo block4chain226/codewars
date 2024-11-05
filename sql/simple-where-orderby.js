fd2c950a22c41af2c79d96dd11f40768c71e2598
ivan <block4ain226@gmail.com>
2024-11-05 16:07:24 +0200

    first commit

---
+++ sql/simple-where-orderby.js
@@ -0,0 +1,3 @@
+// For this challenge you need to create a simple SELECT statement that will return all columns from the people table WHERE their age is over 50
+
+select id, name, age from people where age > 50 order by age desc

010f1c3834dc4375e98a565548b2051b428703b8
ivan <block4ain226@gmail.com>
2024-11-05 19:11:09 +0200

    select with like

---
+++ sql/length-based-select-with-like.js
@@ -0,0 +1,3 @@
+// You will need to create SELECT statement in conjunction with LIKE.Please list people which have first_name with at least 6 character long
+
+select first_name as first_name, last_name as last_name from names where first_name like '______%'

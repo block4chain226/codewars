65bf8d4f6f426752980e2cee215b62f0847fbfe0
ivan <block4ain226@gmail.com>
2024-11-06 19:26:31 +0200

    Given a payment table, which is a part of DVD Rental Sample Database, with the following schema

---
+++ sql/conditional-count.js
@@ -0,0 +1,13 @@
+
+//Given a payment table, which is a part of DVD Rental Sample Database, with the following schema
+
+select extract(month from payment_date) as month,
+    count(payment_id) as total_count,
+    sum(amount) as total_amount,
+    sum(case when staff_id=1 then 1 else 0 end) as mike_count,
+    sum(case when staff_id=1 then amount else 0 end)::NUMERIC as mike_amount,
+    sum(case when staff_id=2 then 1 else 0 end) as jon_count,
+    sum(case when staff_id=2 then amount else 0 end)::NUMERIC as jon_amount
+from payment
+group by month
+order by month asc
